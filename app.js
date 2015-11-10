aebbe7306df3164449bd4822ad7026b71234ee7d
Victor Tang <victortang1992@gmail.com>
2015-11-09 16:46:11 -0800

    set up cells, console logs clicked cells

--- app.js
+++ app.js
@@ -12,7 +12,26 @@ app.factory('memory', function(){
 
 app.controller('MainCtrl', ['$scope', 'memory', function($scope, memory){
     $scope.storage = memory; // load service
-
+    $scope.player = "X";
+    $scope.AI = "O";
+    $scope.turn = 1;
+    $scope.Xed = [];
+    $scope.Oed = [];
+    $scope.blanks = [1,2,3,4,5,6,7,8,9];
+    $scope.draw = function(cellNum){
+        if ($scope.Xed.indexOf(cellNum) == -1){
+            $scope.blanks.splice($scope.blanks.indexOf(cellNum),1);
+            $scope.Xed.push(cellNum);
+        }
+        console.log($scope.Xed);
+        console.log($scope.Oed);
+        console.log($scope.blanks);
+    }
+    
+    
+    
+    
+    
 }]);//end of controller
   //end of function
 })();
