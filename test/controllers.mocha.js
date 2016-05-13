f01ae8014cc8cac1d1b1b590d0b72c4d18b1544d
Victor Tang <victortang1992@gmail.com>
2016-05-13 16:20:34 -0700

    rework test. test passes for now

--- test/controllers.mocha.js
+++ test/controllers.mocha.js
@@ -1,4 +1,4 @@
-describe('saving a document', function() {
+describe('Tic Tac Toe Game: ', function() {
 
   var scope;
   var ctrl;
@@ -11,71 +11,65 @@ describe('saving a document', function() {
   }));
 
 
-  describe('game', function() {
+  describe('Game', function() {
+
+	//always reset board
+	//make sure player is X and computer is O
+	beforeEach(function() {
+		scope.resetBoard();
+		scope.player = 2;
+		scope.computer = 1;
+	});
 	  
 	  //X and O should start blank, Blanks should be full
-    it('should be empty, no one should have any numbers', function() {
-      expect(scope.Xed).to.equal([]);
-      expect(scope.Oed).to.equal([]);
-      assert(scope.blanks).to.deep.equal([1,2,3,4,5,6,7,8,9]);
+    it('should start with a blank board', function() {
+      expect(scope.Xed).to.deep.equal([]);
+      expect(scope.Oed).to.deep.equal([]);
+      expect(scope.blanks).to.deep.equal([1,2,3,4,5,6,7,8,9]);
     });
 
-	  //assume player is X
-	  describe('game where player is X', function() {
+	//should be player's turn first
+	it("should be the Player's turn first", function() {
+	  expect(scope.whoseTurn).to.equal(1);
+	});
+
+	describe('X plays 5, O plays 7', function() {
 
 		//make sure player is X and computer is O
 		beforeEach(function() {
-		  scope.player = 2;
-		  scope.computer = 1;
-		});
-		 
-		//should be player's turn first
-		it("should be the Player's turn first", function() {
-		  expect(scope.whoseTurn).to.equal(1);
+
 		});
 
-		  describe('X plays 5, O plays 7', function() {
+		//if player clicks, 5, 5 should be played by X
+		it('should mark X on 5 if Player played 5', function() {
+		});
 
-			//make sure player is X and computer is O
-			beforeEach(function() {
-			  scope.player = 2;
-			  scope.computer = 1;
-			});
+		//if computer player clicks, 7, 7 should be played by O
+		it('should mark O on 7 if Computer Player played 7', function() {
+		});
+	});
 
-			//if player clicks, 5, 5 should be played by X
-			it('should mark X on 5 if Player played 5', function() {
-			});
+	describe('AI', function() {
 
-			//if computer player clicks, 7, 7 should be played by O
-			it('should mark O on 7 if Computer Player played 7', function() {
-			});
-		  });
+		//make sure game state is blank for each test here
+		beforeEach(function() {
+		});
 
-		  describe('AI', function() {
+		//
+		it('should play opposite corner if player starts with a corner move', function() {
 
-			//make sure game state is blank for each test here
-			beforeEach(function() {
-			  scope.player = 2;
-			  scope.computer = 1;
-			});
+		});
 
-			//
-			it('should play opposite corner if player starts with a corner move', function() {
+		//
+		it('should play a corner if player starts with a center move', function() {
 
-			});
+		});
 
-			//
-			it('should play a corner if player starts with a center move', function() {
+		//
+		it('should play center if player derps and plays a side', function() {
 
-			});
+		});
 
-			//
-			it('should play center if player derps and plays a side', function() {
-
-			});
-
-		  });
-
-	  });
+	});
   });
 });
