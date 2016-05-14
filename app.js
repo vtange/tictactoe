9a1027ccf2806905acca24b16d5d38821385f176
Victor Tang <victortang1992@gmail.com>
2016-05-13 18:07:15 -0700

    revamp AI for testability

--- app.js
+++ app.js
@@ -74,9 +74,7 @@ function MainCtrl($scope, alertify){
             }
         }
     };
-    $scope.computerTurn = function(){
-        $scope.whoseTurn = 2;//Computer Turn, if win, computer wins
-        var alreadyMoved = false;//1 move per computer turn. Change to true once moved.
+    $scope.computerThink = function(){
         var victoryConditions = [[1,2,3],[4,5,6],[7,8,9],[9,6,3],[8,5,2],[7,4,1],[1,5,9],[7,5,3]];
         var justNeed = [[100]];  // offense priority, 100 is placeholder so .sort has something to sort against
         var mustBlock = [[100]]; // defense priority, 100 is placeholder so .sort has something to sort against
@@ -121,58 +119,54 @@ function MainCtrl($scope, alertify){
             //movement phase
             //win first. check for any available attack blanks, attack and Moved=true
                 for (var k=1;k<justNeed.length;k++){//cycle attack options
-                        if (alreadyMoved == false && $scope.blanks.indexOf(parseInt(justNeed[k].join(""),10)) != -1){
-                            $scope.compDraw(parseInt(justNeed[k].join(""),10));
-                            alreadyMoved = true;
+                        if ($scope.blanks.indexOf(parseInt(justNeed[k].join(""),10)) != -1){
+                            return parseInt(justNeed[k].join(""),10);
                         }
                 }
             //defend second, check for any available defense blanks, defend and Moved=true
                 for (var k=1;k<mustBlock.length;k++){//cycle defense options
-                        if (alreadyMoved == false && $scope.blanks.indexOf(parseInt(mustBlock[k].join(""),10)) != -1){
-                            $scope.compDraw(parseInt(mustBlock[k].join(""),10));
-                            alreadyMoved = true;
+                        if ($scope.blanks.indexOf(parseInt(mustBlock[k].join(""),10)) != -1){
+                            return parseInt(mustBlock[k].join(""),10);
                         }
                 }
             //if 5 is played by opponent, play a corner (1 should always be avail)
-				if (opponent.length === 1 && opponent[0] === 5 && alreadyMoved == false){
-					$scope.compDraw(1);
-					alreadyMoved = true;
+				if (opponent.length === 1 && opponent[0] === 5){
+					return 1;
 				}
             //if corner is played by opponent, play opposite corner
-				if (opponent.length === 1 && opponent[0]%2!==0 && alreadyMoved == false){
-					$scope.compDraw(function(){
+				if (opponent.length === 1 && opponent[0]%2!==0){
 					switch(opponent[0]){
 						case 1: return 9;
 						case 3: return 7;
 						case 7: return 3;
 						case 9: return 1;
-					}}());
-					alreadyMoved = true;
+					};
 				}
             //otherwise, if 5 is not played yet, play 5
-				if ($scope.blanks.indexOf(5) != -1 && alreadyMoved == false){
-					$scope.compDraw(5);
-					alreadyMoved = true;
+				if ($scope.blanks.indexOf(5) != -1){
+					return 5;
 				}
 			//play nearest corner to opponent
-				if (opponent.length < 3 && opponent[0]%2===0 && alreadyMoved == false){
-					$scope.compDraw(function(){
+				if (opponent.length < 3 && opponent[0]%2===0){
 					switch(opponent[0]){
 						case 2: return 1;
 						case 4: return 3;
 						case 6: return 7;
 						case 8: return 9;
-					}}());
-					alreadyMoved = true;
+					};
 				}
             //choose a random index from the available scope.blanks
-            if (alreadyMoved == false){
-                $scope.compDraw($scope.blanks[Math.floor(Math.random() * ($scope.blanks.length))]);
-            }
-		
+                return $scope.blanks[Math.floor(Math.random() * ($scope.blanks.length))];
+
+    };
+	$scope.computerTurn = function(){
+		//Computer Turn, if win, computer wins
+        $scope.whoseTurn = 2;
+		//draw what the thinking phase outputs
+		$scope.compDraw($scope.computerThink());
 		//end phase
 		$scope.endPhase();
-    };
+	};
     $scope.checkClickable = function(cellnum){
         if ($scope.blanks.indexOf(cellnum) != -1){ return true; }else{return false;};//for css pointer mouse
     };
