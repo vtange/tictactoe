57d0665ae80993a2dd6c0c242e569f2749cd8b4c
Victor Tang <victortang1992@gmail.com>
2016-05-13 18:53:40 -0700

    AI and victory tests

--- test/controllers.mocha.js
+++ test/controllers.mocha.js
@@ -25,10 +25,18 @@ describe('Tic Tac Toe Game: ', function() {
       expect(scope.blanks).to.deep.equal([1,2,3,4,5,6,7,8,9]);
     });
 
+	//really now, nothing should be played yet
+	it('nothing should be played yet', function() {
+		expect(scope.checkX(5)).to.equal(false);
+		expect(scope.checkO(5)).to.equal(false);
+		expect(scope.checkClickable (5)).to.equal(true);
+	});
+
 	//should be player's turn first
 	it("should be the Player's turn first", function() {
 	  expect(scope.whoseTurn).to.equal(1);
 	});
+
 	describe('Drawing', function() {
 
 		beforeEach(function() {
@@ -90,21 +98,90 @@ describe('Tic Tac Toe Game: ', function() {
 
 	describe('AI', function() {
 
-		//make sure game state is blank for each test here
+		//make sure player is X and computer is O
 		beforeEach(function() {
+			scope.player = 2;
+			scope.computer = 1;
+		});
+		describe('Player starts with 1', function() {
+			beforeEach(function() {
+				scope.draw(1);
+			});
+
+			it('should have made a move', function() {
+				expect(scope.Oed.length).to.not.equal(0);
+			});
+
+			it('should have played 9 since player played 1', function() {
+				expect(scope.Oed[0]).to.equal(9);
+			});
+		});
+		describe('Player starts with 2', function() {
+			beforeEach(function() {
+				scope.draw(2);
+			});
+
+			it('should have made a move', function() {
+				expect(scope.Oed.length).to.not.equal(0);
+			});
+
+			it('should have played 5 since player did not play 5', function() {
+				expect(scope.Oed[0]).to.equal(5);
+			});
+			
+			describe('Player then plays 4', function() {
+				beforeEach(function() {
+					scope.draw(4);
+				});
+
+				it('should have made a move', function() {
+					expect(scope.Oed.length).to.not.equal(1);
+				});
+
+				it('should have played 1 since player played 2 in first turn', function() {
+					expect(scope.Oed[1]).to.equal(1);
+				});
+				describe('Player then plays 6', function() {
+					beforeEach(function() {
+						scope.draw(6);
+					});
+
+					it('should have made a move', function() {
+						expect(scope.Oed.length).to.not.equal(2);
+					});
+				});
+			});
+		});
+		describe('Player starts with 3', function() {
+			//swap players
+			beforeEach(function() {
+				scope.player = 1;
+				scope.computer = 2;
+				scope.draw(3);
+			});
+
+			it('should have made a move', function() {
+				expect(scope.Xed.length).to.not.equal(0);
+			});
+
+			it('should have played 7 since player played 7', function() {
+				expect(scope.Xed[0]).to.equal(7);
+			});
+		});
+		describe('Player starts with 5', function() {
+			beforeEach(function() {
+				scope.draw(5);
+			});
+
+			it('should have made a move', function() {
+				expect(scope.Oed.length).to.not.equal(0);
+			});
+
+			it('should have played 1 since player played 5', function() {
+				expect(scope.Oed[0]).to.equal(1);
+			});
 		});
 
-		it('should play opposite corner if player starts with a corner move', function() {
-
-		});
-
-		it('should play a corner if player starts with a center move', function() {
-
-		});
-
-		it('should play center if player derps and plays a side', function() {
-
-		});
 
 	});
 
@@ -123,7 +200,7 @@ describe('Tic Tac Toe Game: ', function() {
 			expect(scope.OWon()).to.equal(false);
 		});
 
-		describe('Somebody Wins', function() {
+		describe('O Wins', function() {
 
 			beforeEach(function() {
 				//watch resetBoard
@@ -142,7 +219,39 @@ describe('Tic Tac Toe Game: ', function() {
 			});
 
 		});
+		describe('Computer X Wins', function() {
 
+			beforeEach(function() {
+				//watch resetBoard
+				sinon.stub(scope, 'resetBoard', function() {});
+				scope.player = 1;
+				scope.computer = 2;
+				scope.whoseTurn = 2;
+				scope.Xed = [1,2,3];
+				scope.Oed = [5,7,8];
+				scope.checkVictoryFull();
+			});
+
+			it('X just won', function() {
+				expect(scope.XWon()).to.equal(true);
+			});
+
+		});
+
+		describe('Draw', function() {
+
+			beforeEach(function() {
+				//watch resetBoard
+				sinon.stub(scope, 'resetBoard', function() {});
+				scope.blanks = [];
+				scope.checkVictoryFull();
+			});
+
+			it('should reset board if draw', function() {
+				expect(scope.resetBoard.callCount).to.equal(1);
+			});
+
+		});
 	});
   });
 });
